949fea527afead3f85d75827eeb5b2074489dc43
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2026-02-17 23:52:09 +0100

    removed unnecessary signals

--- lex/core/signals/CalculationSignals.py
+++ lex/core/signals/CalculationSignals.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from django.contrib.auth.models import User
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver, Signal
 
@@ -14,20 +14,6 @@ from lex.core.models.CalculationModel import CalculationModel
 from lex.core.signals.ActiveCalculationStateStore import ActiveCalculationStateStore
 
 logger = logging.getLogger(__name__)
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    # ensures profile.save() runs even on updates
-    instance.profile.save()
-
-
 
 def update_calculation_status(
     instance,
