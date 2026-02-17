52fccd5aa84b34ca5bfdfebfcdfca327a0366507
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2026-02-17 02:41:40 +0100

    fixed init command, updated frontend, decreased package size, fixed restframework api key migration problem

--- lex/react/build/config.js
+++ lex/react/build/config.js
@@ -5,3 +5,4 @@ window.REACT_APP_STORAGE_TYPE = undefined
 window.REACT_APP_DOMAIN_BASE = undefined
 window.REACT_APP_PROJECT_DISPLAY_NAME = undefined
 window.REACT_APP_GRAFANA_DASHBOARD_URL = undefined
+window.REACT_APP_STREAMLIT_URL = undefined
