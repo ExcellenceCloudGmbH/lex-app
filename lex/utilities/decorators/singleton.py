2ea55f65b69985004ba152d524ecf9e36b2b6843
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2026-03-28 20:17:17 +0100

    fixed singleton pickling error

--- lex/utilities/decorators/singleton.py
+++ lex/utilities/decorators/singleton.py
@@ -1,13 +1,31 @@
-from functools import wraps
-
-
 def LexSingleton(cls):
-    instances = {}
+    """
+    Turn ``cls`` into a singleton without replacing it with a function.
 
-    @wraps(cls)
-    def get_instance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
+    The previous implementation returned a factory function, which meant the
+    module-level name no longer pointed at the class object. That breaks
+    pickling for instances of decorated classes because pickle resolves
+    ``module.ClassName`` back to the wrapper function instead of the class.
+    """
 
-    return get_instance
+    class SingletonWrapper(cls):
+        _singleton_instance = None
+        _singleton_initialized = False
+
+        def __new__(inner_cls, *args, **kwargs):
+            if inner_cls._singleton_instance is None:
+                inner_cls._singleton_instance = super(SingletonWrapper, inner_cls).__new__(inner_cls)
+            return inner_cls._singleton_instance
+
+        def __init__(self, *args, **kwargs):
+            if self._singleton_initialized:
+                return
+            super().__init__(*args, **kwargs)
+            self._singleton_initialized = True
+
+    SingletonWrapper.__name__ = cls.__name__
+    SingletonWrapper.__qualname__ = cls.__qualname__
+    SingletonWrapper.__module__ = cls.__module__
+    SingletonWrapper.__doc__ = cls.__doc__
+
+    return SingletonWrapper
