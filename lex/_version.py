65e56196d032b3fed94a47866906dbb92d793ceb
Hazem Sahbani <hazemsahbani@gmail.com>
2026-04-09 23:40:52 +0200

    Fixed pipeline with pipy

---
+++ lex/_version.py
@@ -0,0 +1,4 @@
+# This file is overwritten at build time by the CI pipeline.
+# The version is derived from the GitHub release tag.
+# For local development, this fallback is used.
+__version__ = "0.0.0.dev0"
